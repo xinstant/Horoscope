dfd6dccbbe7747163ae1c5d57570067d917a345b
xinstant <66279078+xinstant@users.noreply.github.com>
2021-01-06 11:50:42 -0500

    Update Horoscope.py

--- Horoscope.py
+++ Horoscope.py
@@ -17,7 +17,7 @@ zodiacs = {'Aries':1,'Taurus':2,'Gemini':3,'Cancer':4,'Leo':5,
            'Virgo':6,'Libra':7,'Scorpio':8,'Sagittarius':9,
            'Capricorn':10,'Aquarius':11,'Pisces':12}
 
-def horoscope():
+def zodiac():
     x = input('Type in your zodiac sign: ')
     url = 'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign={}'
     res = requests.get(url.format(zodiacs[x]))
